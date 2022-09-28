7cd26913e75c22bd782c86c64ab23e9e1bfd8d7a
semperubique <lastmurname@gmail.com>
2022-09-28 12:44:36 +0200

    Update

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const cardWrapper = document.querySelector(".card-wrapper");
+const newBook = document.querySelector('#new_book');
+const form = document.querySelector(".modal");
+const closeForm = document.querySelector('#close-form');
+const submitBook = document.querySelector('#submit-book');
 
 
 let myLibrary = [];
 
 function Book(name, author, year) {
-  this.nam = name
+  this.name = name
   this.author = author
   this.year = year
 }
@@ -14,15 +18,36 @@ function addBookToLibrary(name, author, year) {
 }
 
 function displayLibraryOnScreen() {
+    cardWrapper.textContent = "";
     myLibrary.forEach(function(book) {
         let card = document.createElement('div');
         card.classList.add('card');
-        card.textContent = "book";
+        card.innerHTML = 
+        `<h3>${book.name}</h3>
+        <span>By: ${book.author}</span>
+        <span>Published: ${book.year}</span>
+        `;
         cardWrapper.appendChild(card);
     })
 }
 
-addBookToLibrary("Ishmael","Lambard A.", 1922);
-addBookToLibrary("Granderoba","Shebzul B.", 1933);
+function newBookForm() {
+    form.classList.add('active');
+}
 
-displayLibraryOnScreen();
+
+displayLibraryOnScreen(); // test to see if it works always after adding a new book
+
+
+newBook.addEventListener('click', () => newBookForm());
+
+closeForm.addEventListener('click', () => {
+    form.classList.remove('active');
+})
+
+submitBook.addEventListener('click', () => {
+    addBookToLibrary(document.querySelector('#name').value, document.querySelector('#author').value, document.querySelector('#year').value);
+    form.classList.remove('active');
+    displayLibraryOnScreen()
+});
+
